d4e84b0c76652f9a42ac6cccc6410639fb5bc09e
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2023-02-12 16:18:27 -0600

    Changes to the color

--- vpTest/resources/views/app.blade.php
+++ vpTest/resources/views/app.blade.php
@@ -42,19 +42,11 @@
                   </div>
                   <div class="text-center pt-1 mb-12 pb-1">
                     <button
-                      class="inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
+                      class="bg-gradient-to-r from-rose-300 to-rose-500 inline-block px-6 py-2.5 text-white font-medium text-xs leading-tight uppercase rounded shadow-md hover:bg-blue-700 hover:shadow-lg focus:shadow-lg focus:outline-none focus:ring-0 active:shadow-lg transition duration-150 ease-in-out w-full mb-3"
                       type="button"
                       data-mdb-ripple="true"
                       data-mdb-ripple-color="light"
-                      style="
-                        background: linear-gradient(
-                          to right,
-                          #ee7724,
-                          #d8363a,
-                          #dd3675,
-                          #b44593
-                        );
-                      "
+            
                     >
                       Log in
                     </button>
@@ -75,10 +67,8 @@
               </div>
             </div>
             <div
-              class="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none"
-              style="
-                background: linear-gradient(to right, #ee7724, #d8363a, #dd3675, #b44593);
-              "
+              class="lg:w-6/12 flex items-center lg:rounded-r-lg rounded-b-lg lg:rounded-bl-none bg-gradient-to-r from-rose-300 to-rose-500"
+           
             >
               <div class="text-white px-4 py-6 md:p-12 md:mx-6">
                 <h4 class="text-xl font-semibold mb-6">About Meridian Brands</h4>
