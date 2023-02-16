2298c14fd4648929cf282d3eb78b4a6eb74ed4e2
lmdang3 <89552436+lmdang3@users.noreply.github.com>
2023-02-15 19:13:29 -0600

    login is done, this is done by reading in env variables as opposed to using database, thats a future improvement at the moment

--- vpTest/resources/views/logout.blade.php
+++ vpTest/resources/views/logout.blade.php
@@ -1,11 +1,41 @@
 <!DOCTYPE html>
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
 <html>
-    <head>
-        <title>Logout</title>
-    </head>
-    <body>
-        <h1>Logged out successfully!</h1>
-        <p>Thank you for using our application. You have been logged out.</p>
-    </body>
+
+<head>
+    <title>Logout</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
+
+<section id="backgroundcolor" class="text-gray-700 body-font h-90vh lg:h-screen">
+    <!-- This sets the height to 90% of the 
+    viewport height for screens smaller than lg, and to 100% of the viewport height for screens lg and larger. -->
+    <div class="container mx-auto flex px-6 py-24 md:flex-row flex-col items-center container-narrow">
+
+        <div
+            class="lg:flex-grow md:w-2/3 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+            <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">You Have Successfully Logged Out
+            </h1>
+            </h1>
+            <p class="mb-8 leading-relaxed">
+                <!-- Insert some text here if you want -->
+                Please log in to access the portal
+            </p>
+            <div class="flex justify-center">
+            <form action='/login' method="get">
+                    <button type="submit" id="logoutButton"
+                    class="inline-flex text-white border-0 py-2 px-6 focus:outline-nonetext-lg rounded-full">
+                    Login</button>
+                </form>
+            </div>
+        </div>
+        <div class="lg:max-w-lg lg:w-full md:w-1/2 w-5/6 ">
+
+            <img class="w-30 md:w-50 lg:w-100" src="{{asset('assets/images/PersonShoppingLogo.jpg')}}" />
+        </div>
+    </div>
+</section>
+@include('footer')
+
 </html>
--- vpTest/resources/views/navbar.blade.php
+++ vpTest/resources/views/navbar.blade.php
@@ -1,25 +1,15 @@
-<link href="{{ asset('css/app.css') }}" rel="stylesheet">
-
 <!-- <head>
   @vite('resources/css/app.css')
 </head> -->
 
 <nav class="flex items-center justify-between flex-wrap bg-gray-800 p-6">
-    <div class="flex items-center flex-shrink-0 text-white mr-6">
-        <span class="font-semibold text-xl tracking-tight">My App</span>
-    </div>
-    <div class="block lg:hidden">
-        <button
-            class="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
-            <svg class="fill-current h-3 w-3" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg">
-                <title>Menu</title>
-                <path d="M0 3h20v2H0V3zm0 5h20v2H0V8zm0 5h20v2H0v-2z" />
-            </svg>
-        </button>
-    </div>
-    <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+
+    <img src="{{asset('assets/images/Meridian.jpg')}}" class="object-contain hover:object-scale-down" width="10%"
+        height="10%" style="filter: brightness(0) invert(1);">
+
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
         <div class="text-sm lg:flex-grow">
-            <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">
+            <a href="#" class="ml-12 block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">
                 Home
             </a>
             <a href="#" class="block mt-4 lg:inline-block lg:mt-0 text-gray-300 hover:text-white mr-4">
@@ -30,8 +20,9 @@
 
             <form action="/logout" method="POST">
                 @csrf
-                <button type="submit"
-                    class="bg-red-500 hover:bg-red-700 text-white font-bold py-2 px-4 rounded">Logout</button>
+                <button type="submit" id="logoutButton"
+                    class="inline-flex text-white border-0 py-2 px-6 focus:outline-nonetext-lg rounded-full">
+                    Logout</button>
             </form>
 
 
